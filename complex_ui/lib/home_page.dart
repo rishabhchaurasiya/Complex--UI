f21718667e6c2597e8daf148fb4a05db83ced067
RISHABH <rishabhchaurasiya20899@gmail.com>
2022-12-28 23:57:52 +0530

    start

---
+++ complex_ui/lib/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'colors.dart' as color;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: color.AppColor.homePageBackground,
+      body: Container(
+        padding: const EdgeInsets.only(
+          top: 70,
+          left: 30,
+        ),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  'Training',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: color.AppColor.homePageTitle,
+                    fontWeight: FontWeight.w700,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- complex_ui/lib/main.dart
+++ complex_ui/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:complex_ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: const HomePage(),
     );
   }
 }
